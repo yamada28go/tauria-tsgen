2d2679ec23726f8e9c089e90792f4f25fdb95d63
yamada28go <yamada28go@gmail.com>
2025-07-23 23:17:55 +0900

    イベント関連の処理を追加

--- test/tauri-sample/src/app/app.component.ts
+++ test/tauri-sample/src/app/app.component.ts
@@ -5,49 +5,49 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { createLib, MainWindowEventHandlers } from './external/tauri-api';
+import { createLib } from './external/tauri-api';
 import { ILib } from './external/tauri-api/interface/commands/Lib';
 import { open } from '@tauri-apps/plugin-dialog';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { MatExpansionModule  } from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { UnlistenFn, Event } from '@tauri-apps/api/event';
-
+import { TauriMainWindowEventHandlers } from './external/tauri-api/tauria-api/events/TauriMainWindowEventHandlers';
+import { TauriGlobalEventHandlers } from './external/tauri-api/tauria-api/events/TauriGlobalEventHandlers';
 
 // --- Global Window ---
 
-
-class ImpGlobal implements GlobalEventHandlers {
-  constructor() {
+class ImpGlobal extends TauriGlobalEventHandlers {
+  constructor(private snackBar: MatSnackBar) {
     super();
   }
 
-  OnGlobal(event: Event<string>): void {
-    debugger;
-    console.log(event.payload);
-  }
+     OnGlobal(event: Event<string>): void{
+      this.snackBar.open(`Global Event: ${event.payload}`, 'Close', { duration: 3000 });
+     }
+
+
 }
 
 // --- Main Window ---
 
-
-class ImpHoge extends MainWindowEventHandlers {
+class ImpWindow extends TauriMainWindowEventHandlers {
   override OnCoreMsg(event: Event<string>): void {
-    throw new Error('Method not implemented.');
+    this.snackBar.open(`Core Message: ${event.payload}`, 'Close', { duration: 3000 });
   }
-  constructor() {
+  constructor(private snackBar: MatSnackBar) {
     super();
   }
 
   OnLoggedIn(event: Event<string>): void {
     debugger;
     console.log(event.payload);
+    this.snackBar.open(`Logged In: ${event.payload}`, 'Close', { duration: 3000 });
   }
 }
 // --- Main Window ---
 
-const l = new ImpHoge();
-const g = new ImpGlobal();
 
 @Component({
   selector: 'app-root',
@@ -61,12 +61,18 @@ const g = new ImpGlobal();
     FormsModule,
     MatCheckboxModule,
     MatExpansionModule,
+    MatSnackBarModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   lib: ILib;
+
+  // イベントハンドラ
+  private  eventWindows ;
+private  eventGlobal ;
+
 
   // greet_command
   greetName = '';
@@ -98,8 +104,23 @@ export class AppComponent {
   complexStateNumber = 1;
   complexStateResult = '';
 
-  constructor() {
+  // call_msg_global
+  callMsgGlobalResult = '';
+
+  // call_msg_main
+  callMsgMainResult = '';
+
+  constructor(private snackBar: MatSnackBar) {
     this.lib = createLib();
+    this.eventWindows = new ImpWindow(this.snackBar);
+    this.eventGlobal = new ImpGlobal(this.snackBar);
+  }
+
+async  ngOnDestroy() {
+
+   await this.eventWindows.Unlisten();
+   await this.eventGlobal.Unlisten();
+
   }
 
   async greet() {
@@ -167,7 +188,13 @@ export class AppComponent {
     const result = await this.lib.complexStateCommand(this.complexStateNumber);
     this.complexStateResult = JSON.stringify(result, null, 2);
   }
-}
-function getCurrentWebviewWindow() {
-  throw new Error('Function not implemented.');
+
+  async callMsgGlobal() {
+    this.callMsgGlobalResult = await this.lib.callMsgGlobal();
+  }
+
+  async callMsgMain() {
+    this.callMsgMainResult = await this.lib.callMsgMain();
+  }
+
 }
